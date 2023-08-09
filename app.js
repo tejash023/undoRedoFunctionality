4b128b9c31c4908e3a4f5740a0fa0494210d7364
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-08-10 02:24:50 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const inputElement = document.getElementById("input");
+
+let inputArray = [];
+let historyArray = [];
+
+inputElement.addEventListener("input", function () {
+  const inputValue = inputElement.value;
+  inputArray = inputValue.split("");
+  console.log(inputArray);
+  console.log(historyArray);
+});
+
+//if user press "B" Button
+document.addEventListener("keydown", function (event) {
+  // Check if the pressed key is "Backspace"
+  if (event.key === "Backspace" && inputArray.length > 0) {
+    event.preventDefault();
+    console.log("Backspace/Undo is pressed");
+
+    undo(inputArray);
+
+    console.log("undo InputArray", inputArray);
+    console.log("undo HistoryArray", historyArray);
+  } else if (event.key === "z" && event.ctrlKey) {
+    event.preventDefault();
+    console.log("CTRL + Z is pressed");
+
+    redo(historyArray);
+
+    console.log("redo InputArray", inputArray);
+    console.log("redo HistoryArray", historyArray);
+  }
+});
+
+//undo
+function undo(inputArray) {
+  const undoCharacter = inputArray.pop();
+  historyArray.push(undoCharacter);
+}
+
+//redo
+function redo(historyArray) {
+  const redoCharacter = historyArray.pop();
+  inputArray.push(redoCharacter);
+}
